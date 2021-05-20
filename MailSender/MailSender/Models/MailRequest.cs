46397b032b52febf6ddd7bdb7bf20aa840a93460
Shigera Ilia <ShigeraIlia@gmail.com>
2021-05-20 16:01:23 +0300

    final

--- MailSender/MailSender/Models/MailRequest.cs
+++ MailSender/MailSender/Models/MailRequest.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MailSender.Models
 {
     public class MailRequest
     {
+        [Required]
         public string ToEmail { get; set; }
         public string Subject { get; set; }
+
+        [Required]
         public string Body { get; set; }
         public List<IFormFile> Attachments { get; set; }
     }
