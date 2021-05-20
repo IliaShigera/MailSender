282b153ce53f7395c28b8e80a030cbadd1b1a0fe
Shigera Ilia <ShigeraIlia@gmail.com>
2021-05-20 15:25:10 +0300

    add Login and Logout.

--- MailSender/MailSender/Controllers/AccountController.cs
+++ MailSender/MailSender/Controllers/AccountController.cs
@@ -51,5 +51,45 @@ namespace MailSender.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl) =>
+           View(new LoginViewModel(returnUrl));
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
