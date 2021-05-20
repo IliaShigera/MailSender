3fe0f14aac25b45e339529d13dbc5358749fefc7
Shigera Ilia <ShigeraIlia@gmail.com>
2021-05-20 14:51:39 +0300

    add models and context

---
+++ MailSender/MailSender/Models/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailSender.Models
+{
+    public class AppContext : IdentityDbContext<User>
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) =>
+            Database.EnsureCreated();
+    }
+}
---
+++ MailSender/MailSender/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MailSender.Models
+{
+    public class User : IdentityUser
+    {
+        public User() { }
+
+        public User(string email, string name) =>
+            (Email, UserName) = (email, name);
+    }
+}
